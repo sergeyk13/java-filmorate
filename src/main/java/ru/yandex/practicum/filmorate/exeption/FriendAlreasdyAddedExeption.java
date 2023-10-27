dd146d2b27eb50a3c678c0c860d6e46a978cebc0
Sergey <45943782+sergeyk13@users.noreply.github.com>
2023-10-27 10:53:12 +0300

    Revert "Add friends likes"

--- src/main/java/ru/yandex/practicum/filmorate/exeption/FriendAlreasdyAddedExeption.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exeption;
-
-public class FriendAlreasdyAddedExeption extends RuntimeException {
-    public FriendAlreasdyAddedExeption(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/yandex/practicum/filmorate/exeption/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.filmorate.exeption;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
-
